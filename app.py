18efa6fcb110647fbd64dbb44fd9eca4139fa2bf
Kassie Pietsch <112716045+kassiepietsch@users.noreply.github.com>
2024-04-15 19:06:18 -0500

    updated app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ CORS(app)
 # main index page (root route)
 @app.route("/")
 def home():
-    return "<h1>Salary Prediction API</h1><p>BAIS:3300 - Digital Product Development</p><p>Kassie Pietsch</p>"
+    return "<h1>Salary Prediction API</h1><p>BAIS:3300 - Digital Product Development</p><p>Kassie Pietsch & Trevor Bottom</p>"
 
 
 # predict route
